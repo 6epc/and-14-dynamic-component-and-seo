718694dee187b134e3099fc6904c8131a18f736d
Dmitriy Tararoshchenko <para6epc@gmail.com>
2022-10-03 15:43:22 +0300

    head seo tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 import { ModalComponent } from './modal/modal.component';
 import { modalData, ModalDirective } from './modal/modal.directive';
 
@@ -8,6 +9,8 @@ import { modalData, ModalDirective } from './modal/modal.directive';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  @ViewChild(ModalDirective) refModalDirctive!: ModalDirective;
+
   modalData: modalData = {
     title: 'This is a title',
     text: `
@@ -21,7 +24,16 @@ export class AppComponent {
 
   modalComponent = ModalComponent;
 
-  @ViewChild(ModalDirective) refModalDirctive!: ModalDirective;
+  constructor(
+    private title: Title,
+    private meta: Meta
+  ) {
+    this.title.setTitle('Dynamic component and SEO opt.');
+    this.meta.addTags([
+      { name: 'keywords', content: 'angular, google, seo' },
+      { name: 'description', content: 'This is app component' }
+    ])
+  }
 
   showModal(): void {
     this.refModalDirctive.showModal();
